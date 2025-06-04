d4ec74642e9ddf4c340de8b822f5004a24df6cc2
paopao4619 <6665453@qq.com>
2025-06-05 00:19:41 +0800

    改为自己的ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["124.220.218.208"];
+pub const RS_PUB_KEY: &str = "wvKDxZLIl5HxmeSYnk+v7d01eGY+N0BFtN6TxoyYHTc=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
